e778a7bef94a34471c93d8b18eec9f73cc2b8095
Plvkssh <kksmartregion@gmail.com>
2024-12-11 12:36:15 +0700

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+ public class Calculator {
+         public int add(int a, int b) {
+             return a + b;
+         }
+
+         public static void main(String[] args) {
+             Calculator calculator = new Calculator();
+             System.out.println("2 + 3 = " + calculator.add(2, 3));
+         }
+     }
+     
